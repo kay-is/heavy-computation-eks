1ed17dd4caabc4db3f6fcbc8351ae156916eef74
EC2 Default User <ec2-user@ip-172-31-18-32.eu-west-1.compute.internal>
2020-10-07 13:54:12 +0000

    Initial commit

---
+++ bin/heavy-computation-eks.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+const cdk = require('@aws-cdk/core');
+const { HeavyComputationEksStack } = require('../lib/heavy-computation-eks-stack');
+
+const app = new cdk.App();
+new HeavyComputationEksStack(app, 'HeavyComputationEksStack');
